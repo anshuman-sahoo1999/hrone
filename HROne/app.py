01a71ade747cd822c52e4be7b4f18048991236d2
Er. Anshuman Sahoo <61726230+anshuman-sahoo1999@users.noreply.github.com>
2026-01-10 16:22:33 +0530

    Update app.py

--- HROne/app.py
+++ HROne/app.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Route to serve the Single Page Application
+# 1. Main Route
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# 2. Add this "Kiosk" route that ALSO loads index.html
+@app.route('/kiosk')
+def kiosk_view():
+    return render_template('index.html') 
+
 if __name__ == '__main__':
-    # Running on port 5000
     app.run(debug=True, port=5000)
